3d2a4afe3cc18d59c99b94eca600fce8dcf76f92
Yashkalra12 <yash1578.be21@chitkara.edu.in>
2024-05-02 03:08:58 +0530

    Frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,118 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/app.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { Toaster } from "react-hot-toast";
+import { Protected, Public, Admin } from "./middleware/route";
+import React, { lazy, Suspense } from "react";
+import Loading from "./components/Loading";
+
+const Home = lazy(() => import("./pages/Home"));
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Appointments = lazy(() => import("./pages/Appointments"));
+const Doctors = lazy(() => import("./pages/Doctors"));
+const Profile = lazy(() => import("./pages/Profile"));
+const Notifications = lazy(() => import("./pages/Notifications"));
+const ApplyDoctor = lazy(() => import("./pages/ApplyDoctor"));
+const Error = lazy(() => import("./pages/Error"));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Toaster />
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route
+            path="/login"
+            element={<Login />}
+          />
+          <Route
+            path="/register"
+            element={
+              <Public>
+                <Register />
+              </Public>
+            }
+          />
+          <Route
+            path="/"
+            element={<Home />}
+          />
+          <Route
+            path="/doctors"
+            element={<Doctors />}
+          />
+          <Route
+            path="/appointments"
+            element={
+              <Protected>
+                <Appointments />
+              </Protected>
+            }
+          />
+          <Route
+            path="/notifications"
+            element={
+              <Protected>
+                <Notifications />
+              </Protected>
+            }
+          />
+          <Route
+            path="/applyfordoctor"
+            element={
+              <Protected>
+                <ApplyDoctor />
+              </Protected>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <Protected>
+                <Profile />
+              </Protected>
+            }
+          />
+          <Route
+            path="/dashboard/users"
+            element={
+              <Admin>
+                <Dashboard type={"users"} />
+              </Admin>
+            }
+          />
+          <Route
+            path="/dashboard/doctors"
+            element={
+              <Admin>
+                <Dashboard type={"doctors"} />
+              </Admin>
+            }
+          />
+          <Route
+            path="/dashboard/appointments"
+            element={
+              <Protected>
+                <Dashboard type={"appointments"} />
+              </Protected>
+            }
+          />
+          <Route
+            path="/dashboard/applications"
+            element={
+              <Protected>
+                <Dashboard type={"applications"} />
+              </Protected>
+            }
+          />
+          <Route
+            path="*"
+            element={<Error />}
+          />
+        </Routes>
+      </Suspense>
+    </Router>
   );
 }
 
