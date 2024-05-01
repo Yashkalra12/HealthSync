a4a16df073e2f63dce3f5584447db796194adfe4
Yashkalra12 <yash1578.be21@chitkara.edu.in>
2024-05-02 02:52:54 +0530

    backend

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+require("./db/conn");
+const userRouter = require("./routes/userRoutes");
+const doctorRouter = require("./routes/doctorRoutes");
+const appointRouter = require("./routes/appointRoutes");
+const path = require("path");
+const notificationRouter = require("./routes/notificationRouter");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use("/user", userRouter);
+app.use("/doctor", doctorRouter);
+app.use("/appointment", appointRouter);
+app.use("/notification", notificationRouter);
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
